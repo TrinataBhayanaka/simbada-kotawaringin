ac048e332af8c1a9eb64ecab5bfbbb9ca728f453
andre.cun <andre.hadiyono@gmail.com>
2016-02-01 14:15:10 +0700

    revis tgl perubahan--> bukan dari tanggal perolehan tapi tanggal kontrak

--- module/perolehan/kontrak_postingKapitalisasi.php
+++ module/perolehan/kontrak_postingKapitalisasi.php
@@ -40,7 +40,7 @@ while ($dataSP2D = mysql_fetch_assoc($sql)){
     $NilaiPerolehan = ceil($data['NilaiPerolehan'] + $bop);
     $satuan = ceil(intval($data['Satuan']) + ($bop/$data['Kuantitas']));
 
-    $tglKontrak=$data['tglKontrak'];
+    $tglKontrak=$noKontrak['tglKontrak'];
     $updateAset = mysql_query("UPDATE aset SET NilaiPerolehan = '{$NilaiPerolehan}', Satuan = '{$satuan}' WHERE Aset_ID = '{$data['Aset_ID']}'");
     $updateKapital = mysql_query("UPDATE kapitalisasi SET n_status = '1', nilai = if(nilai is null,0,nilai)+{$bop}  WHERE idKontrak = '{$noKontrak['id']}' AND asetKapitalisasi = '{$data['Aset_ID']}'");
   }  
