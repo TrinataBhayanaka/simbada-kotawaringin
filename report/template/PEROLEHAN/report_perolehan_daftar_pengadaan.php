3e06034d0dde53690bedd364539959676ded0e25
ilmawan <ilmawan.muharam@gmail.com>
2018-02-22 08:35:36 +0700

    update revisi lap pengadaan dan module mutasi

--- report/template/PEROLEHAN/report_perolehan_daftar_pengadaan.php
+++ report/template/PEROLEHAN/report_perolehan_daftar_pengadaan.php
@@ -40,13 +40,18 @@ $data=array(
 
 $REPORT_RETRIEVE=new RETRIEVE_REPORT();
 
+/*
+revisi 
+1. list kontrak id by skpd
+*/
 //pengadaan kontrak aset non kapitalisasi
-$data_non_kapitalisasi=$REPORT_RETRIEVE->daftar_pengadaan_berdasarkan_skpd($skpd_id,$tglawalperolehan,$tglakhirperolehan);
-//pr($data_non_kapitalisasi);
+$data = $REPORT_RETRIEVE->test($skpd_id,$tglawalperolehan,$tglakhirperolehan);
+/*$data_non_kapitalisasi=$REPORT_RETRIEVE->daftar_pengadaan_berdasarkan_skpd($skpd_id,$tglawalperolehan,$tglakhirperolehan);
+pr($data_non_kapitalisasi);
 
 //pengadaan kontrak aset kapitalisasi
 $data_kapitalisasi=$REPORT_RETRIEVE->daftar_pengadaan_kapitalisasi_berdasarkan_skpd($skpd_id,$tglawalperolehan,$tglakhirperolehan);
-//pr($data_kapitalisasi);
+pr($data_kapitalisasi);
 $data =array();
 if($data_non_kapitalisasi != '' && $data_kapitalisasi != ''){
 	$data = array_merge($data_non_kapitalisasi,$data_kapitalisasi);
@@ -56,7 +61,7 @@ if($data_non_kapitalisasi != '' && $data_kapitalisasi != ''){
 	$data = $data_kapitalisasi;
 }elseif($data_non_kapitalisasi == '' && $data_kapitalisasi == ''){
 	$data ='';
-}
+}*/
 //pr($data);
 //exit;
 $tglPerolehanAwal=  format_tanggal($tglawalperolehan);
@@ -64,10 +69,9 @@ $tglPerolehanAkhir=  format_tanggal($tglakhirperolehan);
 $tglcetak=  format_tanggal($tglcetak);
 
 $gambar = $FILE_GAMBAR_KABUPATEN;
-$html=$REPORT->report_daftar_pengadaan($data, $gambar,$tglPerolehanAwal,$tglPerolehanAkhir,$tglcetak) ;
+$html=$REPORT->report_daftar_pengadaan_rev($data, $gambar,$tglPerolehanAwal,$tglPerolehanAkhir,$tglcetak) ;
 //pr($html);
-//exit;
-
+//exit();
 if($tipe==1){
 $REPORT->show_status_download_kib();
 $mpdf=new mPDF('','','','',15,15,16,16,9,9,'L');
