16204c1adac199e1eb981ed076db56aae05e061a
andre.hadiyono <andre.hadiyono@gmail.com>
2018-03-27 16:02:01 +0700

    rekap dindik

--- report/template/PEROLEHAN/rekap_rincian_detail_v3.php
+++ report/template/PEROLEHAN/rekap_rincian_detail_v3.php
@@ -3154,8 +3154,8 @@ function history_aset($kodesatker, $aset_id, $tglakhirperolehan, $tglawalperoleh
                 $nilaiPrlhnMutasiTambahFix = ($nilaiPrlhnMutasiTambah);
                 $ASET_ID_CEK_PENGHAPUSAN=$valRwyt->Aset_ID;
                 $key_penghapusan="$ASET_ID_CEK_PENGHAPUSAN-$paramKd_Rwyt";
-                $CEK_DATA_PENGHAPUSAN[$ASET_ID_CEK_PENGHAPUSAN]+=1;
-                if($CEK_DATA_BARU[$key]>1){
+                $CEK_DATA_PENGHAPUSAN[$key_penghapusan]+=1;
+                if($CEK_DATA_PENGHAPUSAN[$key]>1){
                     $nilaiPrlhnMutasiKurang = 0;
                 }else{
                     $nilaiPrlhnMutasiKurang = $valRwyt->NilaiPerolehan;
